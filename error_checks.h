ed8128b02a4b738ef143ae4821eee16c83ee1e3b
Zachary Teper <zachary.teper@gmail.com>
2022-03-05 21:28:48 -0500

    Increase maximum allowed current draw of PiCAM

--- error_checks.h
+++ error_checks.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 
 // From bus line. At this current, a warning will be sent out over CAN
-#define CAM_OVERCURRENT_THRESHOLD_mA 300
+#define CAM_OVERCURRENT_THRESHOLD_mA 500
 
 // General board status checkers
 bool check_bus_current_error(void);
